a5953469eb03dc4ee46957f1df8977932ad1e365
dredACR <49780579+dredACR@users.noreply.github.com>
2026-02-22 15:22:04 +0200

    Refactor UAKino plugin structure and functions

--- uakino.js
+++ uakino.js
@@ -1,21 +1,16 @@
 (function () {
     'use strict';
 
-    // Повідомлення для перевірки, що плагін хоча б завантажився
-    setTimeout(function() {
-        if (window.Lampa) Lampa.Noty.show('UAKino: Плагін активний. Шукайте в картці фільму!');
-    }, 3000);
-
-    function UAKinoPlugin() {
+    function startPlugin() {
         var network = new Lampa.Reguest();
         var base_url = 'https://uakino.best/';
 
-        this.search = function (query, callback) {
+        // Функція пошуку
+        function search(query, callback) {
             var url = base_url + 'index.php?do=search&subaction=search&story=' + encodeURIComponent(query);
             network.native(url, function (html) {
                 var items = [];
-                var cards = $(html).find('.movie-item');
-                cards.each(function () {
+                $(html).find('.movie-item').each(function () {
                     var a = $(this).find('a').first();
                     if (a.attr('href')) {
                         items.push({
@@ -26,9 +21,10 @@
                 });
                 callback(items);
             }, function () { callback([]); });
-        };
+        }
 
-        this.extract = function (movie_url, callback) {
+        // Функція витягування відео
+        function extract(movie_url, callback) {
             network.native(movie_url, function (html) {
                 var iframe = html.match(/src="(https:\/\/ashdi\.vip\/vod\/[^"]+)"/);
                 if (iframe) {
@@ -38,35 +34,30 @@
                     });
                 }
             });
-        };
-    }
+        }
 
-    function start() {
-        var uakino = new UAKinoPlugin();
-
-        // Перехоплюємо побудову сторінки фільму
+        // Слухаємо відкриття картки фільму
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var render = e.object.render();
                 
-                // Якщо кнопка вже є - виходимо
-                if ($('.view--uakino', render).length > 0) return;
+                if (render.find('.view--uakino').length > 0) return;
 
-                // Створюємо кнопку через стандартний метод Lampa
+                // Створюємо кнопку через стандартний клас Lampa
                 var btn = $('<div class="full-start__button selector view--uakino"><span>UAKino (UA)</span></div>');
 
                 btn.on('hover:enter', function () {
                     var title = e.data.movie.title || e.data.movie.name;
                     Lampa.Loading.start();
-                    uakino.search(title, function (res) {
+                    search(title, function (res) {
                         Lampa.Loading.stop();
-                        if (res.length) {
+                        if (res.length > 0) {
                             Lampa.Select.show({
-                                title: 'Результати UAKino',
+                                title: 'UAKino: ' + title,
                                 items: res,
                                 onSelect: function (item) {
                                     Lampa.Loading.start();
-                                    uakino.extract(item.url, function (url) {
+                                    extract(item.url, function (url) {
                                         Lampa.Loading.stop();
                                         Lampa.Player.play({ url: url, title: item.title });
                                     });
@@ -75,33 +66,33 @@
                                     Lampa.Controller.toggle('full_start');
                                 }
                             });
-                        } else Lampa.Noty.show('Нічого не знайдено');
+                        } else {
+                            Lampa.Noty.show('Нічого не знайдено');
+                        }
                     });
                 });
 
-                // --- СИЛОВИЙ МЕТОД ВСТАВКИ ДЛЯ GOOGLE TV ---
-                // Ми будемо кожну секунду перевіряти, чи з'явився блок кнопок, 
-                // і вставимо кнопку, як тільки він буде готовий.
-                var interval = setInterval(function() {
-                    var buttons_block = render.find('.full-start__buttons');
-                    if (buttons_block.length > 0) {
-                        clearInterval(interval);
-                        buttons_block.append(btn);
-                        // Оновлюємо навігацію
+                // Вставка кнопки з невеликою затримкою для Sharp TV
+                setTimeout(function() {
+                    var container = render.find('.full-start__buttons');
+                    if (container.length > 0) {
+                        container.append(btn);
                         Lampa.Controller.toggle('full_start');
                     }
-                }, 100);
-
-                // Зупиняємо через 5 секунд, щоб не навантажувати ТВ
-                setTimeout(function() { clearInterval(interval); }, 5000);
+                }, 1000);
             }
         });
     }
 
-    var wait = setInterval(function() {
-        if (typeof Lampa !== 'undefined' && Lampa.Listener) {
-            clearInterval(wait);
-            start();
-        }
-    }, 500);
+    // Запуск плагіна
+    if (window.Lampa) {
+        startPlugin();
+    } else {
+        var timer = setInterval(function() {
+            if (window.Lampa) {
+                clearInterval(timer);
+                startPlugin();
+            }
+        }, 200);
+    }
 })();
