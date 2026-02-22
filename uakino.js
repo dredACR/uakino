04478329fbe0ed3a22449c768b1d6d5144604b3d
dredACR <49780579+dredACR@users.noreply.github.com>
2026-02-22 15:12:04 +0200

    Update UAKino plugin notifications and context menu

--- uakino.js
+++ uakino.js
@@ -3,7 +3,7 @@
 
     // Повідомлення для перевірки
     setTimeout(function() {
-        if (window.Lampa) Lampa.Noty.show('UAKino: Затисніть ОК на фільмі для перегляду!');
+        if (window.Lampa) Lampa.Noty.show('UAKino: Затисніть ОК і шукайте пункт у списку!');
     }, 3000);
 
     function UAKinoPlugin() {
@@ -43,34 +43,47 @@
     function start() {
         var uakino = new UAKinoPlugin();
 
-        // ДОДАЄМО ПУНКТ У КОНТЕКСТНЕ МЕНЮ (При затисканні ОК)
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
-                // Додаємо в контекстне меню (кнопка "Більше" або затискання ОК)
-                e.object.activity.context_menu({
-                    name: 'uakino',
-                    title: 'Дивитися на UAKino (UA)',
-                    icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>'
-                }, function () {
-                    var title = e.data.movie.title || e.data.movie.name;
-                    Lampa.Loading.start();
-                    uakino.search(title, function (res) {
-                        Lampa.Loading.stop();
-                        if (res.length) {
-                            Lampa.Select.show({
-                                title: 'Результати UAKino',
-                                items: res,
-                                onSelect: function (item) {
-                                    Lampa.Loading.start();
-                                    uakino.extract(item.url, function (url) {
-                                        Lampa.Loading.stop();
-                                        Lampa.Player.play({ url: url, title: item.title });
-                                    });
-                                }
+        // Функція для виконання пошуку та запуску
+        var runAction = function(data) {
+            var title = data.movie.title || data.movie.name;
+            Lampa.Loading.start();
+            uakino.search(title, function (res) {
+                Lampa.Loading.stop();
+                if (res.length) {
+                    Lampa.Select.show({
+                        title: 'UAKino: ' + title,
+                        items: res,
+                        onSelect: function (item) {
+                            Lampa.Loading.start();
+                            uakino.extract(item.url, function (url) {
+                                Lampa.Loading.stop();
+                                Lampa.Player.play({ url: url, title: item.title });
                             });
-                        } else Lampa.Noty.show('Нічого не знайдено');
+                        },
+                        onBack: function() {
+                             Lampa.Controller.toggle('context_menu'); // Повертаємо фокус у меню
+                        }
                     });
+                } else Lampa.Noty.show('На UAKino нічого не знайдено');
+            });
+        };
+
+        // Слідкуємо за відкриттям КОНТЕКСТНОГО МЕНЮ
+        Lampa.Listener.follow('contextmenu', function (e) {
+            // Додаємо наш пункт у масив меню, який Lampa збирається малювати
+            if (e.menu) {
+                e.menu.push({
+                    title: 'Дивитися на UAKino (UA)',
+                    subtitle: 'Українська озвучка',
+                    action: 'uakino_play'
                 });
+            }
+        });
+
+        // Слухаємо клік по нашому пункту в меню
+        Lampa.Listener.follow('contextmenu', function (e) {
+            if (e.action == 'uakino_play') {
+                runAction(e.data);
             }
         });
     }
