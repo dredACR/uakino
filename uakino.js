f613c4ee62e12553f23440dd719a1b554efdbb23
dredACR <49780579+dredACR@users.noreply.github.com>
2026-02-22 15:43:22 +0200

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -1,79 +1,77 @@
 (function () {
     'use strict';
 
-    function start() {
+    var start = function () {
         var network = new Lampa.Reguest();
         var base_url = 'https://uakino.best/';
 
-        // Функція для пошуку відео
-        function getUAKinoVideo(title, callback) {
-            var url = base_url + 'index.php?do=search&subaction=search&story=' + encodeURIComponent(title);
-            network.native(url, function (html) {
-                var iframe = html.match(/src="(https:\/\/ashdi\.vip\/vod\/[^"]+)"/);
-                if (iframe) {
-                    network.native(iframe[1], function (v_html) {
-                        var video = v_html.match(/file:'(.*?)'/);
-                        if (video) callback(video[1]);
-                        else Lampa.Noty.show('Відео не знайдено');
-                    });
-                } else {
-                    Lampa.Noty.show('Фільм не знайдено на UAKino');
-                }
-            }, function() {
-                Lampa.Noty.show('Помилка мережі UAKino');
-            });
-        }
-
-        // Додаємо кнопку через Listener
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var render = e.object.render();
                 
-                // Перевірка чи вже є кнопка
-                if (render.find('.view--uakino').length > 0) return;
+                // Перевірка на дублікат без використання jQuery знака $
+                if (render[0].querySelector('.view--uakino')) return;
 
-                // Створюємо кнопку (без складних стилів, щоб не було помилок)
-                var btn = $('<div class="full-start__button selector view--uakino"><span>UAKino (UA)</span></div>');
+                // Створення кнопки через нативний JS
+                var btn = document.createElement('div');
+                btn.className = 'full-start__button selector view--uakino';
+                btn.innerHTML = '<span>UAKino (UA)</span>';
+                btn.style.backgroundColor = '#1a73e8';
+                btn.style.marginRight = '10px';
 
-                btn.on('hover:enter', function () {
+                // Обробка натискання
+                btn.addEventListener('click', function () {
                     var title = e.data.movie.title || e.data.movie.name;
                     Lampa.Loading.start();
+
+                    var search_url = base_url + 'index.php?do=search&subaction=search&story=' + encodeURIComponent(title);
                     
-                    getUAKinoVideo(title, function(url) {
+                    network.native(search_url, function (html) {
                         Lampa.Loading.stop();
-                        Lampa.Player.play({
-                            url: url,
-                            title: title
-                        });
+                        // Шукаємо перше посилання на фільм
+                        var match = html.match(/class="movie-title"><a href="(https:\/\/uakino\.best\/[^"]+)"/);
+                        if (match && match[1]) {
+                            Lampa.Loading.start();
+                            network.native(match[1], function (movie_html) {
+                                Lampa.Loading.stop();
+                                var iframe = movie_html.match(/src="(https:\/\/ashdi\.vip\/vod\/[^"]+)"/);
+                                if (iframe) {
+                                    network.native(iframe[1], function (v_html) {
+                                        var video = v_html.match(/file:'(.*?)'/);
+                                        if (video && video[1]) {
+                                            Lampa.Player.play({ url: video[1], title: title });
+                                        } else { Lampa.Noty.show('Відеофайл не знайдено'); }
+                                    });
+                                } else { Lampa.Noty.show('Плеєр не знайдено'); }
+                            });
+                        } else { Lampa.Noty.show('Фільм не знайдено'); }
+                    }, function() {
+                        Lampa.Loading.stop();
+                        Lampa.Noty.show('Помилка запиту');
                     });
                 });
 
-                // Вставка в блок кнопок
-                var container = render.find('.full-start__buttons');
-                if (container.length > 0) {
-                    container.append(btn);
-                    // Важливо для Android TV: оновлюємо навігацію пульта
-                    Lampa.Controller.add('full_start', {
-                        toggle: function() {},
-                        up: function() {},
-                        down: function() {},
-                        enter: function() {}
-                    });
+                // Вставка кнопки в блок кнопок
+                var container = render[0].querySelector('.full-start__buttons');
+                if (container) {
+                    container.insertBefore(btn, container.firstChild);
+                    // Повідомляємо Lampa про новий елемент для пульта
+                    if (Lampa.Controller.update) Lampa.Controller.update();
                 }
             }
         });
-    }
+    };
 
-    // Безпечний запуск
-    try {
-        if (window.Lampa) {
-            start();
-        } else {
-            Lampa.Listener.follow('app', function (e) {
-                if (e.type == 'ready') start();
-            });
+    // Безпечний запуск без jQuery
+    var checkLampa = setInterval(function () {
+        if (typeof Lampa !== 'undefined') {
+            clearInterval(checkLampa);
+            try {
+                start();
+            } catch (err) {
+                // Виводимо помилку в Noty, щоб ви могли її побачити на екрані
+                if (Lampa.Noty) Lampa.Noty.show('Помилка ініціалізації: ' + err.message);
+            }
         }
-    } catch (err) {
-        console.error("UAKino Error: ", err);
-    }
+    }, 500);
 })();
