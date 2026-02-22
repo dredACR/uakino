b2f31980f1fef57ba43b267ddf135195ddf64b0a
dredACR <49780579+dredACR@users.noreply.github.com>
2026-02-22 14:45:54 +0200

    Update UAKino plugin for global search integration

--- uakino.js
+++ uakino.js
@@ -3,7 +3,7 @@
 
     // Повідомлення про запуск
     setTimeout(function() {
-        if (window.Lampa) Lampa.Noty.show('UAKino: Додано в головне меню!');
+        if (window.Lampa) Lampa.Noty.show('UAKino: Тепер результати з’являться у загальному пошуку!');
     }, 3000);
 
     function UAKinoPlugin() {
@@ -44,58 +44,36 @@
     function start() {
         var uakino = new UAKinoPlugin();
 
-        // 1. ДОДАВАННЯ В ГОЛОВНЕ МЕНЮ (Зліва)
-        Lampa.Component.add('uakino_menu', function(object){
-            var comp = new Lampa.InteractionMain(object);
-            
-            comp.create = function(){
-                this.activity.loader(true);
-                // Тут можна зробити вивід останніх новинок, але поки зробимо просто запуск пошуку
-                Lampa.Input.edit({
-                    value: '',
-                    title: 'Пошук на UAKino'
-                }, function(value){
-                    if(value){
-                        uakino.search(value, function(res){
-                            comp.activity.loader(false);
-                            if(res.length){
-                                Lampa.Select.show({
-                                    title: 'Результати для: ' + value,
-                                    items: res,
-                                    onSelect: function(item){
-                                        Lampa.Loading.start();
-                                        uakino.extract(item.url, function(url){
-                                            Lampa.Loading.stop();
-                                            Lampa.Player.play({ url: url, title: item.title });
+        // ІНТЕГРАЦІЯ В ГЛОБАЛЬНИЙ ПОШУК LAMPA
+        Lampa.Listener.follow('search', function (e) {
+            if (e.type == 'end' && e.query) {
+                uakino.search(e.query, function(results) {
+                    if (results.length > 0) {
+                        // Створюємо окрему секцію в результатах пошуку
+                        var result_items = results.map(function(item) {
+                            return {
+                                title: item.title,
+                                message: 'Дивитися на UAKino (UA)',
+                                onSelect: function() {
+                                    Lampa.Loading.start();
+                                    uakino.extract(item.url, function(url) {
+                                        Lampa.Loading.stop();
+                                        Lampa.Player.play({
+                                            url: url,
+                                            title: item.title
                                         });
-                                    }
-                                });
-                            } else Lampa.Noty.show('Нічого не знайдено');
+                                    });
+                                }
+                            };
                         });
-                    } else Lampa.Activity.backward();
+
+                        // Додаємо результати в кінець списку пошуку
+                        e.object.append({
+                            title: 'UAKino',
+                            items: result_items
+                        });
+                    }
                 });
-            };
-            return comp;
-        });
-
-        // Додаємо пункт "UAKino" у список меню
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
-                var menu_item = $('<li class="menu__item selector" data-action="uakino">' +
-                    '<div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="2" y="2" width="20" height="20" rx="2.18" ry="2.18"></rect><line x1="7" y1="2" x2="7" y2="22"></line><line x1="17" y1="2" x2="17" y2="22"></line><line x1="2" y1="12" x2="22" y2="12"></line><line x1="2" y1="7" x2="7" y2="7"></line><line x1="2" y1="17" x2="7" y2="17"></line><line x1="17" y1="17" x2="22" y2="17"></line><line x1="17" y1="7" x2="22" y2="7"></line></svg></div>' +
-                    '<div class="menu__text">UAKino</div>' +
-                '</li>');
-
-                menu_item.on('hover:enter', function () {
-                    Lampa.Activity.push({
-                        url: '',
-                        title: 'UAKino',
-                        component: 'uakino_menu',
-                        page: 1
-                    });
-                });
-
-                $('.menu .menu__list').append(menu_item);
             }
         });
     }
