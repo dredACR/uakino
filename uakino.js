5336a48e9243f44f12a9db583edfc235988744f5
dredACR <49780579+dredACR@users.noreply.github.com>
2026-02-22 15:37:31 +0200

    Integrate UAKino into video system menu

--- uakino.js
+++ uakino.js
@@ -38,22 +38,48 @@
     function start() {
         var uakino = new UAKinoPlugin();
 
-        // РЕЄСТРАЦІЯ ЯК ОКРЕМОГО ПУНКТУ В КАРТЦІ
+        // ІНТЕГРАЦІЯ ЧЕРЕЗ СИСТЕМНЕ МЕНЮ "ВІДЕО"
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                var render = e.object.render();
                 var title = e.data.movie.title || e.data.movie.name;
+                
+                // Додаємо пункт у список джерел (це працює в APK 1.12.4)
+                if (e.object.activity && e.object.activity.hasOwnProperty('context_menu')) {
+                    e.object.activity.context_menu({
+                        name: 'uakino',
+                        title: 'UAKino (UA)',
+                        icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>'
+                    }, function () {
+                        Lampa.Loading.start();
+                        uakino.search(title, function (res) {
+                            Lampa.Loading.stop();
+                            if (res.length) {
+                                Lampa.Select.show({
+                                    title: 'Результати UAKino',
+                                    items: res,
+                                    onSelect: function (item) {
+                                        Lampa.Loading.start();
+                                        uakino.extract(item.url, function (url) {
+                                            Lampa.Loading.stop();
+                                            Lampa.Player.play({ url: url, title: item.title });
+                                        });
+                                    }
+                                });
+                            } else Lampa.Noty.show('Нічого не знайдено');
+                        });
+                    });
+                }
 
-                // Створюємо кнопку через офіційні класи Lampa для Google TV
-                var btn = $('<div class="full-start__button selector view--uakino"><span>UAKino (UA)</span></div>');
-
-                btn.on('hover:enter', function () {
+                // Також намагаємося додати кнопку в розділ "Відео"
+                var video_btn = $('<div class="full-start__button selector view--uakino"><span>UAKino (UA)</span></div>');
+                video_btn.on('hover:enter', function() {
+                    // Виклик тієї ж логіки пошуку
                     Lampa.Loading.start();
                     uakino.search(title, function (res) {
                         Lampa.Loading.stop();
-                        if (res.length > 0) {
+                        if (res.length) {
                             Lampa.Select.show({
-                                title: 'UAKino: ' + title,
+                                title: 'UAKino',
                                 items: res,
                                 onSelect: function (item) {
                                     Lampa.Loading.start();
@@ -61,39 +87,25 @@
                                         Lampa.Loading.stop();
                                         Lampa.Player.play({ url: url, title: item.title });
                                     });
-                                },
-                                onBack: function() {
-                                    Lampa.Controller.toggle('full_start');
                                 }
                             });
-                        } else {
-                            Lampa.Noty.show('На UAKino нічого не знайдено');
                         }
                     });
                 });
 
-                // Шукаємо місце для вставки. Якщо немає кнопок, вставляємо в меню вкладок
-                var container = render.find('.full-start__buttons');
-                if (container.length > 0) {
-                    container.append(btn);
-                } else {
-                    // Метод для Lampa Lite / Google TV: додаємо в панель вкладок
-                    render.find('.full-tabs').append(btn);
-                }
-                
-                // Оновлюємо контролер
-                Lampa.Controller.toggle('full_start');
+                // Вставка в блок "ВІДЕО"
+                setTimeout(function() {
+                    $('.full-start__buttons', e.object.render()).append(video_btn);
+                }, 100);
             }
         });
     }
 
-    // Чекаємо повної готовності Lampa
     var wait = setInterval(function() {
         if (typeof Lampa !== 'undefined' && Lampa.Listener) {
             clearInterval(wait);
             start();
-            // Повідомлення з'явиться точно, якщо плагін завантажився
-            Lampa.Noty.show('UAKino: Модуль активовано');
+            Lampa.Noty.show('UAKino для Android APK активовано');
         }
     }, 500);
 })();
